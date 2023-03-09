f7535aaf31754f20f9f3c0c111daf5fc7a0ab4c3
Phap Dieu Duong <ddphap@gmail.com>
2023-03-09 17:59:55 +0800

    settings: auto-update preview

--- Source/ExifGlass/SettingsWindow.axaml.cs
+++ Source/ExifGlass/SettingsWindow.axaml.cs
@@ -1,11 +1,29 @@
+/*
+ExifGlass - Standalone Exif tool for ImageGlass
+Copyright (C) 2023 DUONG DIEU PHAP
+Project homepage: https://github.com/d2phap/ExifGlass
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.Platform.Storage;
 using Avalonia.Styling;
+using ExifGlass.ExifTools;
 using System.Threading.Tasks;
 
 namespace ExifGlass;
@@ -22,6 +40,14 @@ public partial class SettingsWindow : Window
         BtnSelectExecutable.Click += BtnSelectExecutable_Click;
         BtnOK.Click += BtnOK_Click;
         BtnCancel.Click += BtnCancel_Click;
+
+        TxtExecutable.TextChanged += ExifToolConfig_Changed;
+        TxtArguments.TextChanged += ExifToolConfig_Changed;
+    }
+
+    private void ExifToolConfig_Changed(object? sender, TextChangedEventArgs e)
+    {
+        TxtPreview.Text = $"{TxtExecutable.Text?.Trim()} {ExifTool.DefaultCommands} {TxtArguments.Text?.Trim()} \"C:\\path\\to\\photo.jpg\"";   
     }
 
 
@@ -68,7 +94,7 @@ public partial class SettingsWindow : Window
         });
         if (filePicker == null || filePicker.Count == 0) return;
 
-        TxtExecutable.Text = filePicker[0].Path.LocalPath;
+        TxtExecutable.Text = $"\"{filePicker[0].Path.LocalPath}\"";
     }
 
     private void BtnOK_Click(object? sender, RoutedEventArgs e)

8b6874e99aae5c09dd9f27e45cdc6f834ece91d3
Phap Dieu Duong <ddphap@gmail.com>
2023-04-11 11:16:19 +0800

    add update model

---
+++ Source/ExifGlass/Settings/UpdateModel.cs
@@ -0,0 +1,40 @@
+﻿/*
+ExifGlass - EXIF metadata viewer
+Copyright (C) 2023 DUONG DIEU PHAP
+Project homepage: https://github.com/d2phap/ExifGlass
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#nullable disable
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExifGlass;
+
+public class UpdateModel
+{
+    public float ApiVersion { get; set; } = 1;
+
+    public string Version { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+
+    [DataType(DataType.Url)]
+    public Uri ChangelogUrl { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime PublishedDate { get; set; }
+
+    public Uri Url { get; set; }
+}
---
+++ Source/ExifGlass/Settings/UpdateService.cs
@@ -0,0 +1,74 @@
+﻿/*
+ExifGlass - EXIF metadata viewer
+Copyright (C) 2023 DUONG DIEU PHAP
+Project homepage: https://github.com/d2phap/ExifGlass
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExifGlass;
+
+public class UpdateService
+{
+    /// <summary>
+    /// Gets the update information
+    /// </summary>
+    public UpdateModel? CurrentReleaseInfo { get; private set; }
+
+
+    /// <summary>
+    /// Gets the value indicates that the current app has a new update
+    /// </summary>
+    public bool HasNewUpdate
+    {
+        get
+        {
+            if (CurrentReleaseInfo == null)
+            {
+                return false;
+            }
+
+            var newVersion = new Version(CurrentReleaseInfo.Version);
+            var currentVersion = Config.AppVersion;
+
+            return newVersion > currentVersion;
+        }
+    }
+
+
+    /// <summary>
+    /// Gets the latest updates
+    /// </summary>
+    /// <returns></returns>
+    public async Task GetUpdatesAsync()
+    {
+        var url = "https://raw.githubusercontent.com/d2phap/ExifGlass/main/update.json";
+
+
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return;
+        }
+
+        using var stream = await response.Content.ReadAsStreamAsync();
+        CurrentReleaseInfo = await JsonEx.ParseJson<UpdateModel>(stream);
+    }
+}
